ecde7bde4700ad1ae3c51f77e9af05306fbd5d23
Enjmateo <enjmateo@yahoo.fr>
2021-11-26 16:51:45 +0100

    database tests

--- src/main/java/com/insa/communication/TextMessage.java
+++ src/main/java/com/insa/communication/TextMessage.java
@@ -5,17 +5,33 @@ import com.insa.utils.*;
 import java.util.*;
 
 public class TextMessage extends ObjectMessage{
-
+    /*
+    public static void main(String[] args) {
+        
+        Data data = new Data();
+        TextMessage msg = new TextMessage(UUID.randomUUID(),UUID.randomUUID(),"Salut c'est un test");
+        Database database = new Database();
+        database.connect();
+        msg.sendToDatabase(database);
+    }
+    */
 
     private String content;
+
+    public String getContent() {
+        return content;
+    }
 
     public TextMessage (UUID sender, UUID receiver, String content) {
         super(sender, receiver);
         this.content = content;
     }
-
+    
     public void sendToDatabase(Database database) {
-        database.executeQuery("INSERT INTO messages (sender, receiver, content) VALUES ("+super.sender.toString()+", "+super.recepter.toString()+" ,"+content+")");
+        //database.executeUpdate("alter table messages alter column sendDate mediumint");
+        //database.executeUpdate("INSERT INTO messages (sender, receiver, sendDate, contentID, messageType) VALUES ('"+super.sender.toString()+"', '"+super.recepter.toString()+"' ,"+super.date.getTime()+", (SELECT max(messageID) FROM text_message)+1 "+",0);");
+        //System.out.println("Seconde requette");
+        //database.executeUpdate("INSERT INTO text_message (messageID, messagePart,content) VALUES((SELECT max(messageID) FROM text_message)+1,null,'"+content+"')");
         // TODO...
 
     }
