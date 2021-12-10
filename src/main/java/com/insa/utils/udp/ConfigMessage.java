f80df65e161106e1db0afef7d3383749f38c6f1a
Enjmateo <enjmateo@yahoo.fr>
2021-12-10 19:11:07 +0100

    fixing udp discovery

--- src/main/java/com/insa/utils/udp/ConfigMessage.java
+++ src/main/java/com/insa/utils/udp/ConfigMessage.java
@@ -11,6 +11,7 @@ public class ConfigMessage extends ObjectMessage {
     }
     public ConfigMessage(){
         super();
+        type = MessageType.NOTIFY;
     }
     public ConfigMessage(String pseudo, MessageType type){
         super();
