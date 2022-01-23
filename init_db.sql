2df40648a2c273f1b422609fd4d0e2de590fd970
Manah7 <manah@gmx.com>
2022-01-23 11:19:18 +0100

    added sql script for admin config

---
+++ init_db.sql
@@ -0,0 +1,33 @@
+--
+-- Message database initialization script
+-- ODD - 2022
+--
+
+USE `my_DB`;
+
+DROP TABLE IF EXISTS `messages`;
+DROP TABLE IF EXISTS `text_message`;
+DROP TABLE IF EXISTS `file_message`;
+
+create table `messages`(
+  `messageID` integer not null auto_increment, 
+  `sender` char(36) not null, 
+  `receiver` char(36) not null, 
+  `sendDate` bigint not null, 
+  `contentID` integer not null, 
+  `messageType` integer, 
+  PRIMARY KEY (`messageID`)
+);
+
+create table `text_message`(
+  `messageID` integer not null, 
+  `messagePart` integer, 
+  `content` varchar(512)
+);
+
+create table `file_message`(
+  `messageID` integer not null, 
+  `fileName` varchar(128) not null, 
+  `fileID` char(36), 
+  `size` integer
+);
